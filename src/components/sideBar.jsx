9d3369b8aec75ced4c351ada54cb5d3ca1a743eb
rachelb456 <137709064+rachelb456@users.noreply.github.com>
2024-01-29 10:48:22 +0200

    Past event - guests cannot be added

--- src/components/sideBar.jsx
+++ src/components/sideBar.jsx
@@ -8,6 +8,7 @@ import { objownerOfEvent } from "../redux/action/ownerOfEventAction";
 import { getList } from "../redux/action/listInvitedAction";
 import axios from "axios";
 import { Box } from "@mui/system";
+import { error } from "./sweetAlert";
 export const Sidebar1 = () => {
   const params = useLocation();
   let d = useDispatch();
@@ -17,36 +18,42 @@ export const Sidebar1 = () => {
   let list = useSelector(n => n.ListInvitedReducer.list);
   const navigate = useNavigate();
   const [searchParams, setSearchParams] = useSearchParams();
-
+  let nowDate = Date.now();
   useEffect(() => {
     debugger
-    
+
     axios.get(`https://localhost:44325/api/Functions/invitedToEventDtoList/${obj.idEventDto}`).then((k) => {
       d(getList(k.data))
       // sessionStorage.setItem('ListOfInvitedPerOwner', JSON.stringify(k.data))
-  })
+    })
 
     d(objownerOfEvent(obj))
-    navigate('showAllInvited', {state: obj});
+    navigate('showAllInvited', { state: obj });
   }, [])
 
 
 
   return <div>
-    
-      
-    <div className="sidebar" style={{marginTop:"87px"}}>
+
+
+    <div className="sidebar" style={{ marginTop: "87px" }}>
 
       {/* <h1>Sidebar</h1> */}
       <ul>
 
-        <li ><NavLink style={{ color: '#3b3a30'}} to={"addInvited"}>הוספת מוזמנים</NavLink></li>
+        {<li><NavLink onClick={(e) => {
+          if (Date.parse(obj.dateOfEventDto) < nowDate) {
+            e.preventDefault();
+            error("תאריך הארוע עבר, לא ניתן להוסיף מוזמנים")
+          
+          }
+        }} style={{ color: '#3b3a30' }} to={"addInvited"}>הוספת מוזמנים</NavLink></li>}
         <li><NavLink style={{ color: '#3b3a30' }} to={"showAllInvited"}  >הצגת המוזמנים</NavLink></li>
         <li><NavLink style={{ color: '#3b3a30' }} to={"showAllInvitedTrue"}>הצגת המוזמנים שאישרו</NavLink></li>
       </ul>
     </div>
-  
-   
+
+
     <Outlet ></Outlet>
   </div>
 
